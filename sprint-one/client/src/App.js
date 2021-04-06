34e63a29496e0d67c042a41fc8bebacbf54c64e2
robertop96 <roberto.manzano96@gmail.com>
2021-04-05 23:56:08 -0400

    create edit warehouse component
    [Ticket: X]

--- sprint-one/client/src/App.js
+++ sprint-one/client/src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        {/* <Header /> */}
         <Switch>
-          <Route path="/" component={HOME} />
-          <Route path="/warhouses" component={Warehouses} />
-          <Route path="/inventory" component={Inventory} />
-          <Route path="/placeholder" component={PLACEHOLDER} />
+          <Route path="/" component={Home} />
+          {/* <Route path="/warhouses" component={Warehouses} /> */}
+          {/* <Route path="/inventory" component={Inventory} /> */}
+          {/* <Route path="/placeholder" component={PLACEHOLDER} /> */}
         </Switch>
       </div>
     </Router>
---
+++ sprint-one/client/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.scss';
+
+import React from 'react';
+import EditWarehouse from '../../components/EditWarehouse/EditWarehouse';
+
+function Home() {
+  return (
+    <section>
+      {/* <h1>NavbarMockup</h1> */}
+      <EditWarehouse />
+    </section>
+  );
+}
+
+export default Home;
