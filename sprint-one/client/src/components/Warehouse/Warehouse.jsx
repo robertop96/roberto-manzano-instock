0ca78098f44af0124a3f24478a6e9c27feefb266
CYrancher23 <77399044+CYrancher23@users.noreply.github.com>
2021-04-08 15:45:11 -0500

    Warehouse component complete

--- sprint-one/client/src/components/Warehouse/Warehouse.jsx
+++ sprint-one/client/src/components/Warehouse/Warehouse.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState, useCallback} from 'react';
 import del from "../../Assets/Icons/delete_outline-24px.svg";
 import edit from "../../Assets/Icons/edit-24px.svg";
-import search from "../../Assets/Icons/search-24px.svg"
+import sort from "../../Assets/Icons/sort-24px.svg"
 import arrow from "../../Assets/Icons/chevron_right-24px.svg"
 import axios from "axios";
 import "./Warehouse.css"
@@ -47,12 +47,33 @@ const Warehouse=(props)=> {
                    <Link> <button className="search__button">+ Add New Warehouse </button></Link>
                 </div>
             </div>
+            <div className="bar">
+                <div className="bar__labels">
+                    <h4>WAREHOUSE</h4>
+                    <img className="bar__arrows--warehouse" src={sort}/>
+                </div>
+                <div className="bar__labels">
+                    <h4>ADDRESS</h4>
+                    <img className="bar__arrows--address" src={sort}/>
+                </div>
+                <div className="bar__labels">
+                    <h4>CONTACT NAME</h4>
+                    <img className="bar__arrows--name" src={sort}/>
+                </div>
+                <div className="bar__labels">
+                    <h4>CONTACT INFORMATION</h4>
+                    <img className="bar__arrows--info" src={sort}/>
+                </div>
+                <div className="bar__labels">
+                    <h4>ACTIONS</h4>
+                </div>
+            </div>
         {responseData?.map((data)=>{
                 return(
                 <div key={data.id}>
                 
                     <div className="card">           
-                        <div>
+                        <div className="card__warehouse">
                             <h4 className="card__label">WAREHOUSE</h4>
                             <div className="arrow__align">
                                 <p className="card__text--name">{data.name}</p>
@@ -68,16 +89,20 @@ const Warehouse=(props)=> {
                             <Link><img className="card__img--delete" src={del} /></Link>
                         </div>
                   
-                        <div>
+                        <div className="card__name">
                             <h4 className="card__label">CONTACT NAME</h4>
                             <p className="card__text">{data.contact.name}</p>
                         </div>
-                        <div>
+                        <div className="card__info">
                             <h4 className="card__label">CONTACT INFORMATION</h4>
                             <p className="card__text--br">{data.contact.phone}</p>
                             <p className="card__text--br">{data.contact.email}</p>
                         </div>
-                        <div>
+                        <div className="card__icons">
+                            <div>
+                            <Link> <img className="card__img--delete2" src={del} />
+                            </Link>
+                            </div>
                            <Link> <img className="card__img--edit" src={edit} /></Link>
                         </div>
                     </div>
