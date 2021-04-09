cdcb8e2a6e84cb1ef806d6f6dbe8f408e6058d5d
orlandollerena <trancetribal@bellsouth.net>
2021-04-09 16:58:15 -0400

    saved index js

--- sprint-one/server/routes/inventory/index.js
+++ sprint-one/server/routes/inventory/index.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 const deleteInventory = require("./deleteInventory");
 const inventoryFunctionExample = require("./deleteInventory");
 const addInventory = require("./addInventory");
+const inventoryList = require("./inventoryList");
 const editInventory = require("./editInventory");
 
+router.get("/list", inventoryList);
 router.get("/", inventoryFunctionExample);
 router.post("/", addInventory);
 router.put("/:id", editInventory);
 router.delete("/:id", deleteInventory);
-
-=======
-const router = require('express').Router();
-const deleteInventory = require('./deleteInventory');
-const inventoryFunctionExample = require('./deleteInventory');
-const addInventory = require('./addInventory');
-const inventoryList = require('./inventoryList')
-
-router.get('/list', inventoryList);
-router.get('/', inventoryFunctionExample);
-router.post('/', addInventory);
-router.delete('/:id', deleteInventory);
->>>>>>> 0038d180deb9e94e3926c59a482b77d11dcec31c
 module.exports = router;
