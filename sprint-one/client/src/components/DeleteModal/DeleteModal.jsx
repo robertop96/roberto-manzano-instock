81ea72dc65228f300bf3cd37950072a56165bb12
malalaalonso <marialauraas03@gmail.com>
2021-04-10 16:08:17 -0400

    last commit

--- sprint-one/client/src/components/DeleteModal/DeleteModal.jsx
+++ sprint-one/client/src/components/DeleteModal/DeleteModal.jsx
@@ -41,12 +41,12 @@ const DeleteModal = ({ data, setShowModal, setResponseData }) => {
 
             <h1 className="modal__title">
               Delete {data.name} {data.itemName}{" "}
-              {pathname === "/warehouse" ? "warehouse" : "inventory item"}?
+              {pathname === "/warehouses" ? "warehouse" : "inventory item"}?
             </h1>
 
             <p className="modal__p">
               Please confirm you want to delete {data.name} from the{" "}
-              {pathname === "/warehouse" ? "warehouse" : "inventory"} list? You
+              {pathname === "/warehouses" ? "warehouse" : "inventory"} list? You
               won't be able to undo this action.
             </p>
           </div>
