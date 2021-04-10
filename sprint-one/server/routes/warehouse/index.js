c5f23311d647837a93d7685d016304bf02dc3759
CYrancher23 <77399044+CYrancher23@users.noreply.github.com>
2021-04-09 19:39:31 -0500

    Delete warehouse backend complete

--- sprint-one/server/routes/warehouse/index.js
+++ sprint-one/server/routes/warehouse/index.js
@@ -4,12 +4,14 @@ const addWarehouse = require("./addWarehouse");
 const WarehouseDetails = require("./WarehouseDetails");
 const warehouseList = require("./warehouseList");
 const editWarehouse = require("./editWarehouse");
+const deleteWarehouse= require("./deleteWarehouse")
 
 router.get("/list/all", warehouseList);
 
 router.get("/:id", WarehouseDetails);
 router.post("/", addWarehouse);
 router.put("/:id", editWarehouse);
+router.delete('/:id', deleteWarehouse);
 
 router.get("/", functionExample);
 
