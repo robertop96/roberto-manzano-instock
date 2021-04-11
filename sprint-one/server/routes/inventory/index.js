39d4f4f2fe06b7f389a224a698449dababdaacfb
Parham <psaniei@hotmail.com>
2021-04-10 22:19:44 -0400

    create single item endpoint

--- sprint-one/server/routes/inventory/index.js
+++ sprint-one/server/routes/inventory/index.js
@@ -4,8 +4,10 @@ const inventoryFunctionExample = require("./deleteInventory");
 const addInventory = require("./addInventory");
 const inventoryList = require("./inventoryList");
 const editInventory = require("./editInventory");
+const inventoryItemDetails = require("./inventoryItemDetails");
 
 router.get("/list", inventoryList);
+router.get("/:id", inventoryItemDetails);
 router.get("/", inventoryFunctionExample);
 router.post("/", addInventory);
 router.put("/:id", editInventory);
